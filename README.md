6b9d43181bd1c0bdb2ee2faf08df8c595aa641c6
Joseph Budiarto <budiartojoseph@gmail.com>
2017-11-16 10:14:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Django-Realtime: an iShout.js client for Django.
+
+iShout.js is a node.js + socket.io server used for adding real-time notifications to your existing web application.
+This project adds easy iShout.js integration for Django, thus enabling you to easily
+add real-time notifications to you exiting Django applications. This is an updated version of django-realtime
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-realtime',
+    version='1.1',
+    author='Joseph Budiarto',
+    author_email='',
+    description='an iShout.js client for Django',
+    url='https://bitbucket.org/inzane/django-realtime',
+    license='',
+    packages=find_packages(),
+    classifiers = [
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django'
+    ]
+)
