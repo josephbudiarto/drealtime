7aadd32fe3b36308833583799c70800ee2364bbb
Joseph Budiarto <budiartojoseph@gmail.com>
2017-11-16 10:43:02 +0100

    Initial commit

--- setup.py
+++ setup.py
@@ -3,15 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name='django-realtime',
     version='1.1',
-    author='Joseph Budiarto',
-    author_email='',
+    author='Anish menon',
+    author_email='anish@inzane.in',
     description='an iShout.js client for Django',
     url='https://bitbucket.org/inzane/django-realtime',
-    license='',
+    license='MIT',
     packages=find_packages(),
     classifiers = [
+        'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Framework :: Django'
